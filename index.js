3367a388bb026743b70ba679d1c4e821a7f5cb26
brian23ka <brianngenye@gmail.com>
2025-03-16 11:19:44 +0300

    using DOM

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainTag = document.getElementById("main");
+console.log(mainTag.remove());
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Arnold Kulavi is the champion";
+document.body.append(newHeader);
+
